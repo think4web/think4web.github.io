13ddffdd407838ccd03f56b674112e17e3eda1ef
think4web <cheverdak.nikolay@gmail.com>
2021-12-01 08:56:26 +0200

    Update titleize_overrride.rb

--- _plugins/titleize_overrride.rb
+++ _plugins/titleize_overrride.rb
@@ -1,7 +1,6 @@
-module Jekyll
-  module Utils
-    def titleize_slug(slug)
-      slug.split(/[_-]/).join(' ').capitalize
-    end
+class Jekyll::Post
+
+  def titleized_slug
+    self.slug.split(/[_-]/).join(' ').capitalize
   end
 end
